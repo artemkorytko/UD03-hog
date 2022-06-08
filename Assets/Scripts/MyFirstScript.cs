7e2a9ac2648ad9846fb363b952d872a740944468
bbinc1 <pashigorev.ilya@gmail.com>
2022-06-08 19:02:28 +0300

    SecondScript

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    int counter = 5;
+    int testCounter = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello world!");
+        do
+        {
+            print("Hellow World");
+            counter = counter + 1;
+        } while (counter < testCounter);
     }
+
 
     // Update is called once per frame
     void Update()
