e8645ac2f143620de0013ec79827ed23ebe6cbf3
Pavel Nikitin <nikitinps92@gmail.com>
2022-06-09 22:15:17 +0300

    Add prefabs and scripts

---
+++ Assets/scripts/GameItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameItem : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Find();
+    }
+
+    private void Find()
+    {
+        Debug.Log($"Find object {gameObject.name}");
+    }
+}
