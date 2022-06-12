f042aada8940e418394b0ecea22c9dba019ae888
Rickitoshi <ginko.98@mail.ru>
2022-06-12 13:54:43 +0300

    Add UI, Level, GameManager, GameItem(+Data), UIItem

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameItem : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Find();
+    }
+
+    private void Find()
+    {
+        Debug.Log($"Find object{gameObject.name}");
+    }
+}
---
+++ Assets/Scripts/GameItemData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameItemData 
+{
+    private Sprite _sprite;
+    private int _amount;
+
+    public GameItemData(Sprite sprite)
+    {
+        _sprite = sprite;
+        _amount = 1;
+    }
+
+    public void IncreaseAmount()
+    {
+        _amount++;
+    }
+
+    public bool DecreaseAmount()
+    {
+        _amount--;
+
+        return _amount > 0;
+    }
+}
